bd5f33e7ef6292764de6c5127dddce582317c75e
Murray King <murray.king@ed.ac.uk>
2013-11-29 16:35:51 +0000

    Add getter to get tiledLayer

--- MapView/Map/RMMapTiledLayerView.h
+++ MapView/Map/RMMapTiledLayerView.h
@@ -36,6 +36,8 @@
 
 @property (nonatomic, readonly) id <RMTileSource> tileSource;
 
+
+
 - (id)initWithFrame:(CGRect)frame mapView:(RMMapView *)aMapView forTileSource:(id <RMTileSource>)aTileSource;
 
 @end
